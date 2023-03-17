32b1ed28607ac8e22794cf1eb0eeed688573c565
George Adrian <103585248+georgeadrila@users.noreply.github.com>
2023-03-17 17:05:25 +0300

    python input

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,71 +1,71 @@
 # NUMBERS
-# x = 1
-# y = 3.393
-# z = 1j
+x = 1
+y = 3.393
+z = 1j
 
-# print(type(x))
-# print(type(y))
-# print(type(z))
+print(type(x))
+print(type(y))
+print(type(z))
 
-# # converting data type
-# a = float(x)
-# print(type(a))
+# converting data type
+a = float(x)
+print(type(a))
 
-# import random
-# print(random.randrange(1,5)*10-4)
+import random
+print(random.randrange(1,5)*10-4)
 
 # python casting is the actual declaration of a variable with int, float and string
 
 # stirngs
 
-# a = """fjnjnkjnfjnjnf
-# jnfljldlkvlk
-# rmfkmkrmkrmfkrmfkmkmkvk
-# rllfmrlffkk  vrmm ediemei"""
-# print(a)
+a = """fjnjnkjnfjnjnf
+jnfljldlkvlk
+rmfkmkrmkrmfkrmfkmkmkvk
+rllfmrlffkk  vrmm ediemei"""
+print(a)
 
 # strings czn be arrays in python
 
 
-# a = "Hello George what a wonderful day"
-# x = "hiio" in a
-# print(x)
+a = "Hello George what a wonderful day"
+x = "hiio" in a
+print(x)
 
-# def func1():
-#     age = 20
-#     date = 2001
-#     txt = "My name is maswankiu and i am {} years old and i was born in {}"
-#     print(txt.format(age, date))
+def func1():
+    age = 20
+    date = 2001
+    txt = "My name is maswankiu and i am {} years old and i was born in {}"
+    print(txt.format(age, date))
 
-# func1()
+func1()
 
 # +++++++++++LISTS++++++++++
-# thislist = ["appe", "mango","pine"]
-# thislist2 = ["appe", "mango","pine"]
-# thislist[1] = "george"
-# thislist.append("baboon")
-# thislist.insert(1, "cartoon")
-# list3 = thislist + thislist2 
+thislist = ["appe", "mango","pine"]
+thislist2 = ["appe", "mango","pine"]
+thislist[1] = "george"
+thislist.append("baboon")
+thislist.insert(1, "cartoon")
+list3 = thislist + thislist2 
 
-# # if "appe" in thislist:
-# print(list3)
+if "appe" in thislist:
+  print(list3)
 
-# tuple = ("jjd", "hhd", "hehe")
-# for x in tuple:
-#     print(tuple)
+tuple = ("jjd", "hhd", "hehe")
+for x in tuple:
+    print(tuple)
 
-# set = {"one", "two", "three"}
-# for x in set:
-#     print(x)
+set = {"one", "two", "three"}
+for x in set:
+    print(x)
 
-# dict = {
-#     "brand": "Ford",
-#     "model": "Mustang",
-#     "year": 1994
-# }
-# # x = dict["brand"]
-# for m, n in dict.items():
-#     print(m, n)
+dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1994
+}
+# x = dict["brand"]
+for m, n in dict.items():
+    print(m, n)
 
 myfamily = {
   "child1" : {
@@ -82,16 +82,21 @@ myfamily = {
   }
 }
 
-# for m , n in myfamily.items():
-#     print(m, n)
+for m , n in myfamily.items():
+    print(m, n)
 
-# print(myfamily.get("child2"))\
+print(myfamily.get("child2"))\
 
-# a = '2'
-# b = '4'
-# print(a+b)
+a = '2'
+b = '4'
+print(a+b)
 
 x = 0
 while x < 20:
   x = x + 3
 print(x)
+
+
+
+
+
---
+++ Day8/day8.py
@@ -0,0 +1,9 @@
+print("Enter: a numnber: ")
+number = int(input())
+print("Enter a second number: ")
+number2 = int(input())
+if number2 < 5:
+    print(number2 + " is less than 5")
+# num = number + number2
+# print("The sum of " + number + " and " + number2 + " is " + num)
+
